07744cd982251801457ae62c8f867e8be6cb0982
ThiagoDePaulaSouza <thiagodepaulasouza@gmail.com>
2023-05-21 15:01:18 -0300

    adicionado Soma Recursiva

---
+++ Soma/Program.cs
@@ -0,0 +1,17 @@
+﻿int Soma(int N, List<int> A)
+{
+    if (N == 0)
+    {
+        return 0;
+    }
+        return Soma(N - 1, A) + A[N];
+}
+
+var lista = new List<int>()
+{
+    1,2, 3, 4, 5, 6, 7, 8, 9, 10,
+};
+
+var resultado1 = Soma(5, lista);
+Console.WriteLine(resultado1);
+Console.ReadLine();
